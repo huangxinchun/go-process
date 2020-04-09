a533ce04bfe874522f5b3c092a73e22adef915d7
hxc <173315279@qq.com>
2020-04-09 18:03:15 +0800

    修改

--- day1/proess-exit/main.go
+++ day1/proess-exit/main.go
@@ -1,5 +1,10 @@
 package main
 
 func main()  {
-	panic("call panic")
+	//panic("call panic")
+	//死锁
+	//死锁(Deadlock)就是一个进程拿着资源A请求资源B，
+	//另一个进程拿着资源B请求资源A，双方都不释放自己的资源，导致两个进程都进行不下去。
+	ch := make(chan int)
+	<-ch
 }
