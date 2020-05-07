58147c20c1285074a9044a73e861828bfbd19dee
hxc <173315279@qq.com>
2020-05-07 17:37:36 +0800

    tst

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test
+
+go 1.14
+
+require github.com/huangxinchun/go-demo v0.0.0-20200506090431-a596009e077b
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/huangxinchun/go-demo/testpackage"
+)
+
+func main() {
+	testpackage.TestNew()
+	fmt.Println("main")
+}
